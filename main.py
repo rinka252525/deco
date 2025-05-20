a31ac5c36c70c9420b200e24606769e7dbd5e648
rinka252525 <110900887+rinka252525@users.noreply.github.com>
2025-05-20 14:47:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -86,8 +86,6 @@ Mid: 110
 Adc: 90
 Sup: 85
 ```"""
-
-"  
     for uid, values in server_data.items():
         total = values['top'] + values['jg'] + values['mid'] + values['adc'] + values['sup']
         msg += f"{values['mention']} -> top: {values['top']}, jg: {values['jg']}, mid: {values['mid']}, adc: {values['adc']}, sup: {values['sup']} | Total: {total}\n"
