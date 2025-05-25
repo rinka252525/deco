cb6cec8977d9b7851e68b2ded61f948f56f44797
rinka252525 <110900887+rinka252525@users.noreply.github.com>
2025-05-26 03:17:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -275,9 +275,8 @@ async def make_teams2(ctx, lane_diff: int = 40, team_diff: int = 50):
                 role_map = {}
                 assigned = set()
 
-#from itertools import permutations
-
-#lanes = ['top', 'jg', 'mid', 'adc', 'sup']
+from itertools import permutations
+lanes = ['top', 'jg', 'mid', 'adc', 'sup']
 
 for team1_ids in combinations(vc_ids, 5):
     team2_ids = [uid for uid in vc_ids if uid not in team1_ids]
