2f21c6fb10839a4a32a3d65344ef1b75f575b693
rinka252525 <110900887+rinka252525@users.noreply.github.com>
2025-05-26 17:32:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -187,6 +187,7 @@ async def join(ctx, *args):
         participants[guild_id] = {}
 
     participants[guild_id][user_id] = preferred_lanes
+    lanes_str = f"{lane1.upper()} / {lane2.upper()}" if lane1 != lane2 else lane1.upper()
     await ctx.send(f"{member.display_name} が {preferred_lanes} で参加登録しました。")
 
 
